39defad7bdd8622679c77513a79bddc205d5facf
famufti <farman.mufti@tintash.com>
2024-10-31 14:11:04 +0500

    no message

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,8 @@ if submit_button:
                 # presigned_url = "https://gen-ai-poc1.s3.amazonaws.com/our-expert-doc-poc/patient/4/prescription/b.jpg?AWSAccessKeyId=ASIAS3UDQMI75MGDO4NI&Signature=ME2sSiyRYgH%2FWF%2FessEg14jiWT8%3D&x-amz-security-token=IQoJb3JpZ2luX2VjEBEaCXVzLXdlc3QtMiJHMEUCIQDlDJPZA7mG7oK3vREPbuv2uKtE19N5NONfnFXkF8YpdAIgMOwg3Eo5%2B8IpqponX1GsZZY4Hzd4ksK%2F87z7dhdFZ7EqhQMIiv%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARADGgwxOTY3NzA1NTQ0MzEiDKoJKaL8jq4mMQ3rfSrZAoZcvtRYRP5cFIVufoWHRyw4q7H4mZxfieJAHlCodXbtQ6leBWwSiY%2FfLOHCnuz6FJGw7hVRVTu%2FLI6eZvq5kAqLR8CB8kjbstmnE%2FV2%2B3i8k4eXrm2ahBHNo%2BeYFhxTeYZmEX3GpohShD9GYEeLjg5xVhs%2FM9I2H7e0tJdVwQRAw3gAO%2FnlHGrr%2Fdh49uL0ZmX%2BB3V4iG4JRnWwxTQxFuiKGVJFr2XGU9C0pI3riEwgTqjvDaFHmX2wWzwEMty%2BQyTwS2xxjGA2BUlmbrNCXRIDXlCdcJI2ksOxCbVWoxDaGC3EbyyYo0l8%2FKuHQssySg7wgSRN4VPHOxnqj39EGwQC6Hv6Wzf6zI5Io9ycVBHN17zwauhJuG7DkDseHjxY3kn1XPAFxAyVuF5yKTJW9xRQiiGEYpyMx62vEK%2FpmEqP1884Ju3lq7SDPOfbUiUhOHiuS5JVp%2Fe5jjCWgI25BjqeAf%2FK%2Fm5On7t8%2B%2FyjZJ%2BiU132KTrx2uo26sG60AGEMRv9UVn1nbsqlTEF8axgc7HBWwrHaGppbwdpKEpE6Quwm1WDo2erAUeRPbUa33GCy8T2oAsdfz0yalAtAzz3%2FCBQeIQyHcmsqN08vFS0Hl53jS%2Fb9lj9zDzxD7eMgJfY%2B%2FXltxtbUmmlOAG8ecoIi%2FQ578fMMqWz%2BtZmTBGReIHC&Expires=1730450172"
                 upload_response = requests.put(presigned_url, data=file.getvalue())
 
-                st.text_input(upload_response)
+                file_name = file.name
+                st.text_input(file_name + " uploaded")
 
         except requests.exceptions.RequestException as e:
             st.error(f"An error occurred: {e}")
